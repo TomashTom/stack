146d1b57e71e75cb6798902fe4079e89aa6235a0
TomashTom <smolskijt@gmail.com>
2021-06-06 22:03:37 +0300

    First commit

---
+++ Stack and Queue/Stack and Queue/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack_and_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> stack = new Stack<string>();
+            stack.Push("a");
+            stack.Push("b");
+            Console.WriteLine(stack.Peek());
+            stack.Pop();
+            Console.WriteLine(stack.Peek());
+
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            Console.WriteLine(queue.Peek());
+            queue.Dequeue();
+            Console.WriteLine(queue.Peek());
+
+
+            Console.ReadLine();
+            
+        }
+    }
+}
